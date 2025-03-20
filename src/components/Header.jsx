b9134fd9b267225abb2074754f37bd06f9493d58
unknown <mohamedsr92@gmail.com>
2025-03-21 00:41:52 +0200

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <>
-        
-    </>
+   <header>
+    <div className="logo">
+        <img src="./html.png" alt="Logo" />
+    </div>
+   </header>
   )
 }
 
