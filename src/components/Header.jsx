dd1b8b244b23bd1321fbc4d0fff43db2ad3b6b80
unknown <mohamedsr92@gmail.com>
2025-03-21 23:08:38 +0200

    1st Header version done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-   <header>
-    <div className="logo">
-        <img src="./html.png" alt="Logo" />
+   <header className="w-full border-blue-200 bg-blue-400">
+   <div className="container mx-auto flex items-center">
+    <div className="logo flex-shrink-0">
+        <img src="./html.png" alt="Logo" className='w-14 h-14' />
+    </div>
+    <nav className="flex-1 flex justify-center">
+      <ul className="flex space-x-18 font-['Raleway'] text-3xl">
+        <li><a href="./" className="font-medium transition-all duration-300 hover:text-white hover:border-b-2 hover:border-white">Home</a></li>
+        <li><a href="./" className="font-medium transition-all duration-300 hover:text-white hover:border-b-2 hover:border-white">Projects</a></li>
+        <li><a href="./" className="font-medium transition-all duration-300 hover:text-white hover:border-b-2 hover:border-white">About</a></li>
+      </ul>
+    </nav>
     </div>
    </header>
   )
